24bad1904ea2bfffb67ea0de8334dce3dc8c4189
lvuk <luka.vuk456@gmail.com>
2023-05-13 18:28:10 +0200

    mobile responsiv fix #2

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -41,17 +41,6 @@ const Register = () => {
       {loading && <Loading />}
       <div className='row justify-content-center align-items-center w-100 h-100'>
         <div className='col-md-5'>
-          <div className='lottie'>
-            <lottie-player
-              src='https://assets9.lottiefiles.com/packages/lf20_06a6pf9i.json'
-              background='transparent'
-              speed='1'
-              loop
-              autoplay
-            ></lottie-player>
-          </div>
-        </div>
-        <div className='col-md-5'>
           <Form layout='vertical' onFinish={onSubmit}>
             <h1>EXPENSE TRACKER - REGISTER</h1>
             <Form.Item label='Name' name='name'>
@@ -72,6 +61,17 @@ const Register = () => {
               </button>
             </div>
           </Form>
+        </div>
+        <div className='col-md-5'>
+          <div className='lottie'>
+            <lottie-player
+              src='https://assets9.lottiefiles.com/packages/lf20_06a6pf9i.json'
+              background='transparent'
+              speed='1'
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
         </div>
       </div>
     </div>
